cd52048a194a6d2e6dd2329212942ae7d705f065
Brandon <brandon@MacBook-Air.lan>
2025-05-20 21:48:12 -0400

    two more components

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+    return (
+        <section id = "hero"
+        className ="relativr min-h-screen flex flex-col items-center justify-center px-4">
+
+
+
+
+            
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { Navbar } from "../components/Navbar";
 import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return (
@@ -9,9 +12,14 @@ export const Home = () => {
 
         <StarBackground />
 
+        <Navbar />
 
+        <main>
+          <HeroSection />
+          
+        </main>
 
-
+        <Footer />
      </div>
     );
 }
